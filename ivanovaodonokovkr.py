e2834a59e5ec60309efe03caed8969466928a29b
opilane <opilane@HP-CZC2349HTM>
2023-02-28 14:33:47 +0200

    Add project files.

---
+++ ivanovaodonokovkr.py
@@ -0,0 +1,18 @@
+
+
+
+from Omamodul import*
+linn=[]
+inimesed=[]
+
+
+while True:       
+    linn, inimesed = linnad()
+    print("Linn:", linnad)
+    print("inimesed:", inimesed)
+    käsk =Käkk(input("käsk:1-Uurige linna nime sisestades, kui palju elanikke selles on/ 2-tähestikuline linnade loetelu ja elanike arv/ 3-Sisestage elanike arv ja kuvage linna nimi, kus on lähim elanikke/ 4-Leidke linnad, kus on vähem kui n elanikku"))
+
+
+
+
+
